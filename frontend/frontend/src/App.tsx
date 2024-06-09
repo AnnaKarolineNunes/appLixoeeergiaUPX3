11d49eb412b30d7b8891a031485053c4c9ba46ff
Gabriel Pereira D Alves <gabriel.pereira.domingos.alves@gmail.com>
2024-06-09 18:45:03 -0300

    Routing

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,17 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "./pages/auth/login";
 import Home from "./pages/home";
-import PontosColeta from "./pages/PontosDeColeta"
-import './globals.css'
+import PontosColeta from "./pages/PontosDeColeta";
 import Register from "./pages/auth/register";
-import PassosColeta from "./pages/Instrução"
+import PassosColeta from "./pages/Instrução";
 import ListaMateriais from "./pages/pesagem/MateriaisPermitidos";
 import SelecionarMateriais from "./pages/pesagem/SelecionarMateriais";
 import InserirResiduos from "./pages/pesagem/SelecionarItens";
 import Pesagem from "./pages/pesagem/Pesagem";
 import ConfirmacaoDesconto from "./pages/pesagem/Desconto";
+import './globals.css';
 
 export function App() {
   return (
-    <ConfirmacaoDesconto />
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/pontos-de-coleta" element={<PontosColeta />} />
+        <Route path="/instrucoes" element={<PassosColeta />} />
+        <Route path="/materiais-permitidos" element={<ListaMateriais />} />
+        <Route path="/selecionar-materiais" element={<SelecionarMateriais />} />
+        <Route path="/selecionar-itens" element={<InserirResiduos />} />
+        <Route path="/pesagem" element={<Pesagem />} />
+        <Route path="/desconto" element={<ConfirmacaoDesconto />} />
+      </Routes>
+    </Router>
+  );
 }
